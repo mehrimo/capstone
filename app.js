dbf5fc552af3a564c9ae53a552b1b227ad1359e4
Mehri Russo <mehrimova@gmail.com>
2017-03-02 11:37:57 -0700

    importing Wikipedia data

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 "use strict";
 
-var CV_URL = "https://vision.googleapis.com/v1/images:annotate?key=" + apiKey;
+var IMG_URL = "https://vision.googleapis.com/v1/images:annotate?key=" + apiKey;
+
+// var WIKI_QUERY = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Aquilegia_caerulea&callback=?";
+
+// colorado blue columbine
+var plantName = "Aquilegia_caerulea"
+
+// colorado blue columbine
+var plantName = "Aquilegia_caerulea"
+
+var WIKI_QUERY = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+plantName+"&callback=?";
 
 var canvas
 var ctx
@@ -74,13 +84,52 @@ function sendFileToCloudVision(content) {
 
   $('#results').text('Loading...');
   $.post({
-    url: CV_URL,
+    url: IMG_URL,
     data: JSON.stringify(request),
     contentType: 'application/json'
   }).fail(function(jqXHR, textStatus, errorThrown) {
     $('#results').text('ERRORS: ' + textStatus + ' ' + errorThrown);
   }).done(displayJSON);
+
+
+
+
+//Wikipedia Link 1
+  // $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?', function(json) {
+  //    $('#wikiInfo').html(json.parse.text["*"]);
+  //    $("#wikiInfo").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");});
+  //    $("#wikiInfo").find("a").attr("target", "_blank");
+  //  });
+
+
+   //Wikipedia Link 2
+  $.ajax({
+        type: "GET",
+        url: WIKI_QUERY,
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "json",
+        success: function (data, textStatus, jqXHR) {
+
+            var markup = data.parse.text["*"];
+            var blurb = $('<div></div>').html(markup);
+
+            // remove links as they will not work
+            blurb.find('a').each(function() { $(this).replaceWith($(this).html()); });
+
+            // remove any references
+            blurb.find('sup').remove();
+
+            // remove cite error
+            blurb.find('.mw-ext-cite-error').remove();
+            $('#article').html($(blurb).find('p'));
+
+        },
+        error: function (errorMessage) {
+        }
+    });
 }
+
 
 /**
  * Displays the results.
@@ -88,18 +137,25 @@ function sendFileToCloudVision(content) {
 function displayJSON(data) {
   console.log(data);
   var contents = JSON.stringify(data, null, 4);
-  var description = data.responses[0].labelAnnotations[2].description;
-  // var wikiCB = " Some info about the flower";
+  var description = [data.responses[0].labelAnnotations[0].description,   data.responses[0].labelAnnotations[1].description ,  data.responses[0].labelAnnotations[2].description]
+  var wikiCB = " Some info about the flower, is it poisonous?";
   $("#results").text(description);
 
+
+  if (description === "flower") {
+    console.log('flower');
+  }
   // $("#results").text(data);
 
 
-  // if (description == 'colorado blue columbine') {
-  //   $("#wiki").text(wikiCB);
-  // }
+  if (description == 'colorado blue columbine') {
+    $("#wiki").text(wikiCB);
+  }
+
 
 }
+
+// if description includes plant or flower move on.
   // console.log(data.responses[0].labelAnnotations[2].description);
   // var description = data.responses[0].labelAnnotations[0].description
 
