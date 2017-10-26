abd541d16e8daeebedf091221bd75b68e2a4a274
mehrimo <mehrimova@gmail.com>
2017-10-26 16:06:00 -0600

    clean up, remove async false

--- public/js/app.js
+++ public/js/app.js
@@ -112,7 +112,6 @@ $("#results").text(descriptionName);
         type: "GET",
         url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page="+wikiSearch+"&callback=?",
         contentType: "application/json; charset=utf-8",
-        async: false,
         dataType: "json",
         success: function (data, textStatus, jqXHR) {
 
